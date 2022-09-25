326e6a874a235f5fd596162a06b2d0ece11ef11f
DongGyu <ehdrb700606@naver.com>
2022-09-25 14:20:03 +0900

    Initial commit

---
+++ src/main/java/com/mountain/springbootmountain/SpringbootMountainApplication.java
@@ -0,0 +1,13 @@
+package com.mountain.springbootmountain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMountainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMountainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mountain/springbootmountain/SpringbootMountainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mountain.springbootmountain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMountainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
