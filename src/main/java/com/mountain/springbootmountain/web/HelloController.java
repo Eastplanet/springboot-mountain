217cab1177060329756ffd284bd0eec81bd60c75
DongGyu <ehdrb700606@naver.com>
2022-09-25 17:51:55 +0900

    .

--- src/main/java/com/mountain/springbootmountain/web/HelloController.java
+++ src/main/java/com/mountain/springbootmountain/web/HelloController.java
@@ -1,20 +1,26 @@
 package com.mountain.springbootmountain.web;
 
-import com.mountain.springbootmountain.web.dto.HelloResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
+    @GetMapping("/")
+    public String welcome(){
+        log.info("home controller");
         return "hello";
     }
 
-    @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount") int amount){
-        return new HelloResponseDto(name,amount);
+    @GetMapping("/callbackpos")
+    public String callback(HttpSession session){
+        log.info("callback controller");
+        return "callback";
     }
+
+
 }
