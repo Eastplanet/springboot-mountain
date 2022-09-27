3179363f849e68ad60f24ffbd91711dd07614936
DongGyu <ehdrb700606@naver.com>
2022-09-27 14:50:56 +0900

    kakao 로그인 테스트

--- src/main/java/com/mountain/springbootmountain/web/HelloController.java
+++ src/main/java/com/mountain/springbootmountain/web/HelloController.java
@@ -1,26 +1,50 @@
 package com.mountain.springbootmountain.web;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 @Slf4j
 public class HelloController {
 
+    @Autowired
+    KaKaoService ks;
+
     @GetMapping("/")
     public String welcome(){
         log.info("home controller");
-        return "map";
+        return "login";
     }
 
     @GetMapping("/map")
     public String map(){
         log.info("map controller");
         return "map";
+    }
+
+    @GetMapping("/kakao")
+    public String getCI(@RequestParam String code, Model model) throws IOException {
+
+        log.info("kakao controller");
+
+        System.out.println("code = " + code);
+        String access_token = ks.getToken(code);
+        Map<String, Object> userInfo = ks.getUserInfo(access_token);
+        model.addAttribute("code", code);
+        model.addAttribute("access_token", access_token);
+        model.addAttribute("userInfo", userInfo);
+
+        //ci는 비즈니스 전환후 검수신청 -> 허락받아야 수집 가능
+        return "welcome";
     }
 
 
